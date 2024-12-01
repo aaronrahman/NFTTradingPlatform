61c66c965fdf4bfe70dd388e17881ecc831625bc
Aaron Rahman <arahm32@uwo.ca>
2024-12-01 07:12:47 -0500

    just adding some stuff to main  Please enter the commit message for your changes. Lines starting

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,8 @@ const basename = import.meta.env.BASE_URL;
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-xxxxxxxx.us.auth0.com" // Replace with your Auth0 domain
-      clientId="your-client-id" // Replace with your Auth0 client ID
+      domain="http://dev-0ktuozmerrxh1e88.us.auth0.com" // Replace with your Auth0 domain
+      clientId="EWStDyW2HHdIIRg2EXxUGV4tfu2PmsUY" // Replace with your Auth0 client ID
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
